17df9835f135673b253e32eddd37dfc7b4e2edd4
Zach Harmon <zacharyharmon02@gmail.com>
2024-01-28 17:25:49 -0600

    Basic Home Page

--- resources/footer.js
+++ resources/footer.js
@@ -3,12 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const footerHTML = `
     <p>
-        <strong>ScreenTime Technologies LLC</strong> is a U.S. based company that specializes in the development of software and hardware solutions for the modern world.
-        <br>We offer a wide range of services, including <strong>web development, mobile application development, and custom software solutions</strong> as well as an ever-growing
-        catalog of <strong>online tools and resources</strong> to help you complete your projects.<br>We are a small company with a big heart, and we are dedicated to providing
+        ScreenTime Technologies LLC is a U.S. based company that specializes in the development of software and hardware solutions for the modern world.
+        <br>We offer a wide range of services, including web development, mobile application development, and custom software solutions as well as an ever-growing
+        catalog of online tools and resources to help you complete your projects.<br>We are a small company with a big heart, and we are dedicated to providing
         the best possible service to our customers. We are always looking for new ways to improve our products and services, so if you have any suggestions
         or feedback, please feel free to contact us.
     </p>
+
+    <div id="footer-quick-links">
+        <a href="../Home/">Home</a>
+        <a href="../About/">About</a>
+        <a href="../Contact/">Contact</a>
+        <a href="../Privacy/">Privacy Policy</a>
+    </div>
 
     <p>
         <strong>
