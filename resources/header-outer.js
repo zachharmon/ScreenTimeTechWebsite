abcf2e67761000191e788b5f8d5f09b470b5fc0d
Zach Harmon <zacharyharmon02@gmail.com>
2024-01-28 15:27:32 -0600

    Basic Site Layout

---
+++ resources/header-outer.js
@@ -0,0 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const routes = [
+        { path: "./Home/", name: "Home" },
+        { path: "./Products/", name: "Products" },
+        { path: "./Tools/", name: "Tools" },
+        { path: "./About/", name: "About" },
+        { path: "./Account/", name: "Account" },
+    ]
+
+    const headerNavLarge = document.querySelector("header#main-header nav#header-nav-large");
+    const headerNavSmall = document.querySelector("header#main-header nav#header-nav-dropdown #header-nav-dropdown-area");
+
+    var headerNavContent = ""
+
+    routes.forEach(route => {
+        headerNavContent += `<a href="${route.path}">${route.name}</a>`
+    });
+
+    headerNavLarge.innerHTML = headerNavContent;
+    headerNavSmall.innerHTML = headerNavContent;
+});
